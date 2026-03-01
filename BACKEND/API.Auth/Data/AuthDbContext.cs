e5011b6c4389d01d1e878b0aaaa55473eebddb9e
thanhtran2005isme-art <thanhtran2005isme@gmail.com>
2026-03-01 11:15:00 +0700

    Them AuthDbContext

---
+++ BACKEND/API.Auth/Data/AuthDbContext.cs
@@ -0,0 +1,18 @@
+using API.Auth.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Auth.Data;
+
+public class AuthDbContext(DbContextOptions<AuthDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(e =>
+        {
+            e.HasKey(u => u.Id);
+            e.HasIndex(u => u.Email).IsUnique();
+        });
+    }
+}
